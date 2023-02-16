071c7782078f5a83871bd2b6c2122c2dd8e5f446
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-02-16 11:02:03 +0100

    creation of R script

---
+++ Wealth_AFR_code.R
@@ -0,0 +1,8 @@
+# PhD project on wealth and age at first reproduction ----
+
+# This script is meant to collect all the necessary code to build up the models and explore the data necessary to understand the relationship between wealth and age at first reproduction.
+
+## Data exploration ----
+
+
+
