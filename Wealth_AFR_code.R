7bee6f119d965554c03f5e6f6ad9f6c85c0cbfdb
Pablo Jose Varas Enriquez <pablo_varas@EVA.MPG.DE>
2023-02-22 18:51:03 +0100

    merged the demographic and wealth databases

--- Wealth_AFR_code.R
+++ Wealth_AFR_code.R
@@ -328,6 +328,61 @@ summary(complete.cases(sample_demo[c("UIyearXhsh95","UIyearXhsh98","UIyearXhsh00
 
 ## Material wealth data ----
 
+#subset wealth data by year
+#1995
+data_wealth_95 <- data_wealth[which(data_wealth$UIyearXhsh > 1995000 & data_wealth$UIyearXhsh < 1996000),]
+#1998
+data_wealth_98 <- data_wealth[which(data_wealth$UIyearXhsh > 1998000 & data_wealth$UIyearXhsh < 1999000),]
+#2000
+data_wealth_00 <- data_wealth[which(data_wealth$UIyearXhsh > 2000000 & data_wealth$UIyearXhsh < 2001000),]
+#2002
+data_wealth_02 <- data_wealth[which(data_wealth$UIyearXhsh > 2002000 & data_wealth$UIyearXhsh < 2003000),]
+#2004
+data_wealth_04 <- data_wealth[which(data_wealth$UIyearXhsh > 2004000 & data_wealth$UIyearXhsh < 2005000),]
+#2006
+data_wealth_06 <- data_wealth[which(data_wealth$UIyearXhsh > 2006000 & data_wealth$UIyearXhsh < 2007000),]
+#2010
+data_wealth_10 <- data_wealth[which(data_wealth$UIyearXhsh > 2010000 & data_wealth$UIyearXhsh < 2011000),]
+
+#change UIyearXhsh name by year
+#1995
+names(data_wealth_95)[names(data_wealth_95) == "UIyearXhsh"] <- "UIyearXhsh95"
+colnames(data_wealth_95) <- c("UIyearXhsh95","acused95","ttsacks95","jjunezero95","LstockValField_rdKts95","HsVal_rdKts95","HsTotAsset_rdKts95","SumValue_rdKts95","IWC95","Loiske_rev95")
+#1998
+names(data_wealth_98)[names(data_wealth_98) == "UIyearXhsh"] <- "UIyearXhsh98"
+colnames(data_wealth_98) <- c("UIyearXhsh98","acused98","ttsacks98","jjunezero98","LstockValField_rdKts98","HsVal_rdKts98","HsTotAsset_rdKts98","SumValue_rdKts98","IWC98","Loiske_rev98")
+#2000
+names(data_wealth_00)[names(data_wealth_00) == "UIyearXhsh"] <- "UIyearXhsh00"
+colnames(data_wealth_00) <- c("UIyearXhsh00","acused00","ttsacks00","jjunezero00","LstockValField_rdKts00","HsVal_rdKts00","HsTotAsset_rdKts00","SumValue_rdKts00","IWC00","Loiske_rev00")
+#2002
+names(data_wealth_02)[names(data_wealth_02) == "UIyearXhsh"] <- "UIyearXhsh02"
+colnames(data_wealth_02) <- c("UIyearXhsh02","acused02","ttsacks02","jjunezero02","LstockValField_rdKts02","HsVal_rdKts02","HsTotAsset_rdKts02","SumValue_rdKts02","IWC02","Loiske_rev02")
+#2004
+names(data_wealth_04)[names(data_wealth_04) == "UIyearXhsh"] <- "UIyearXhsh04"
+colnames(data_wealth_04) <- c("UIyearXhsh04","acused04","ttsacks04","jjunezero04","LstockValField_rdKts04","HsVal_rdKts04","HsTotAsset_rdKts04","SumValue_rdKts04","IWC04","Loiske_rev04")
+#2006
+names(data_wealth_06)[names(data_wealth_06) == "UIyearXhsh"] <- "UIyearXhsh06"
+colnames(data_wealth_06) <- c("UIyearXhsh06","acused06","ttsacks06","jjunezero06","LstockValField_rdKts06","HsVal_rdKts06","HsTotAsset_rdKts06","SumValue_rdKts06","IWC06","Loiske_rev06")
+#2010
+names(data_wealth_10)[names(data_wealth_10) == "UIyearXhsh"] <- "UIyearXhsh10"
+colnames(data_wealth_10) <- c("UIyearXhsh10","acused10","ttsacks10","jjunezero10","LstockValField_rdKts10","HsVal_rdKts10","HsTotAsset_rdKts10","SumValue_rdKts10","IWC10","Loiske_rev10")
+
+#merge with demographic data
+#1995
+x <- merge(data_demo,data_wealth_95,by=c("UIyearXhsh95"),all.x=T)
+#1998
+x <- merge(x,data_wealth_98,by=c("UIyearXhsh98"),all.x=T)
+#2000
+x <- merge(x,data_wealth_00,by=c("UIyearXhsh00"),all.x=T)
+#2002
+x <- merge(x,data_wealth_02,by=c("UIyearXhsh02"),all.x=T)
+#2004
+x <- merge(x,data_wealth_04,by=c("UIyearXhsh04"),all.x=T)
+#2006
+x <- merge(x,data_wealth_06,by=c("UIyearXhsh06"),all.x=T)
+#2010
+x <- merge(x,data_wealth_10,by=c("UIyearXhsh10"),all.x=T)
+
 ### Farming land ----
 
 ### Cash crops ----
